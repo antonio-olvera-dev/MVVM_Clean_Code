5c47f47432dafabe815def43c6e1437fe1834322
Antonio Olvera <antonioolvera1995@gmail.com>
2021-06-06 22:53:52 +0200

    add implementation

--- app/src/main/java/com/toni/mvvm__clean_code/MainActivity.kt
+++ app/src/main/java/com/toni/mvvm__clean_code/MainActivity.kt
@@ -3,9 +3,16 @@ package com.toni.mvvm__clean_code
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity()  {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
+
 }
