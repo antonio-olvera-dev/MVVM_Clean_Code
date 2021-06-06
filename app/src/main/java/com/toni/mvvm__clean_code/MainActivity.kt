8d4b0215947aabae6cecc429436b650ebdb1ccd4
Antonio Olvera <antonioolvera1995@gmail.com>
2021-06-06 22:12:56 +0200

    init

---
+++ app/src/main/java/com/toni/mvvm__clean_code/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.toni.mvvm__clean_code
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
