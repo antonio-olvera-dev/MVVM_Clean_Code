960193fd96c2f933d8ebba8148ca2e1eefb834d9
Antonio Olvera <antonioolvera1995@gmail.com>
2021-06-20 14:06:21 +0200

    create structure of activity_home.xml

--- app/src/main/java/com/toni/mvvm__clean_code/ui/view/HomeView.kt
+++ app/src/main/java/com/toni/mvvm__clean_code/ui/view/HomeView.kt
@@ -2,10 +2,10 @@ package com.toni.mvvm__clean_code.ui.view
 
 import android.app.DatePickerDialog
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
+import com.toni.mvvm__clean_code.R
 import com.toni.mvvm__clean_code.data.article.model.Article
 import com.toni.mvvm__clean_code.databinding.ActivityHomeBinding
 import com.toni.mvvm__clean_code.ui.view.home_controller.HomeController
@@ -17,11 +17,10 @@ class HomeView : AppCompatActivity() {
     private val viewModel: HomeViewModel by viewModels()
     private lateinit var binding: ActivityHomeBinding
     private val controller: HomeController = HomeController()
-     private lateinit var calendar:DatePickerDialog
+    private lateinit var calendar: DatePickerDialog
     private var pressBtnStart: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         val view = binding.root
@@ -32,26 +31,40 @@ class HomeView : AppCompatActivity() {
 
     private fun start() {
 
+        viewModel.articleParametersGet.startDate = getString(R.string.start_date)
+        viewModel.articleParametersGet.endDate = getString(R.string.end_date)
         calendar = DatePickerDialog(this)
 
         calendar.setOnDateSetListener(DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
 
-            val date: String = controller.getBuildDate(year.toString(), month.toString(), dayOfMonth.toString())
+            val date: String =
+                controller.getBuildDate(year.toString(), month.toString(), dayOfMonth.toString())
+
+            if (pressBtnStart) {
+                binding.homeBtStartDate.text = date
+            } else {
+                binding.homeBtEndingDate.text = date
+            }
+
             viewModel.loadParams(pressBtnStart, date)
-
-            Toast.makeText(
-                this,
-                date,
-                Toast.LENGTH_SHORT
-            ).show()
-
         })
 
-        calendar.show()
+        binding.homeBtStartDate.setOnClickListener {
+            pressBtnStart = true
+            calendar.show()
+        }
 
+        binding.homeBtEndingDate.setOnClickListener {
+            pressBtnStart = false
+            calendar.show()
+        }
+
+        binding.ibRocket.setOnClickListener {
+            viewModel.loadArticles()
+        }
 
         viewModel.getArticles().observe(this, Observer<List<Article>> { articles ->
-            binding.tvWelcome.text = articles.toString()
+//            binding.tvWelcome.text = articles.toString()
         })
 
     }
--- app/src/main/java/com/toni/mvvm__clean_code/ui/viewModel/HomeViewModel.kt
+++ app/src/main/java/com/toni/mvvm__clean_code/ui/viewModel/HomeViewModel.kt
@@ -1,6 +1,5 @@
 package com.toni.mvvm__clean_code.ui.viewModel
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -13,12 +12,13 @@ import kotlinx.coroutines.launch
 
 class HomeViewModel : ViewModel(), IArticleParametersGet {
 
-    override val articleParametersGet: ArticleParametersGet = ArticleParametersGet("1995-06-19", "1995-06-25")
-    val useCase: ArticleUseCase = ArticleUseCase()
+    override val articleParametersGet: ArticleParametersGet =
+        ArticleParametersGet("", "")
+    private val useCase: ArticleUseCase = ArticleUseCase()
 
     private val articles: MutableLiveData<List<Article>> by lazy {
         MutableLiveData<List<Article>>().also {
-            loadStartup()
+
         }
     }
 
@@ -27,23 +27,18 @@ class HomeViewModel : ViewModel(), IArticleParametersGet {
         return articles
     }
 
-    private fun loadStartup() {
+    fun loadParams(pressBtnStart: Boolean, date: String) {
+        if (pressBtnStart) articleParametersGet.startDate = date else articleParametersGet.endDate =
+            date
+    }
+
+    fun loadArticles() {
         viewModelScope.launch {
             val newArticles = useCase.getArticles(articleParametersGet)
             if (!newArticles.isNullOrEmpty()) {
                 articles.postValue(newArticles)
             }
         }
-
-    }
-
-    fun loadParams(pressBtnStart: Boolean, date: String) {
-        if (pressBtnStart) articleParametersGet.startDate = date else articleParametersGet.endDate =
-            date
-    }
-
-    fun loadArticles(articleParametersGet: ArticleParametersGet) {
-
     }
 
 
